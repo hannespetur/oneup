8b8da39e42d7d89f8fccbf7dc1e366242c37178e
Loknar <svefgud@gmail.com>
2015-04-03 22:53:20 +0000

    setting up unfinished

--- manage.py
+++ manage.py
@@ -2,9 +2,18 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oneup_django.settings")
+# load environment settings
+try:
+	from environment import environment
+except:
+	environment = ''
 
+if __name__ == "__main__":
+	if environment in ['development','testing','production']:
+    	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings.%s' % environment)
+    else:
+    	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings')
+    
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- oneup/urls.py
+++ oneup/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
-#from django.contrib import admin
+from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'oneup_django.views.home', name='home'),
+    # url(r'^$', 'oneup.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
