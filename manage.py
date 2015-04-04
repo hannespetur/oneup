039cdae6f3131a564327da52e7971a0510c2c1ef
Loknar <svefgud@gmail.com>
2015-04-04 11:47:10 +0000

    whoops

--- manage.py
+++ manage.py
@@ -15,5 +15,4 @@ if __name__ == "__main__":
 		os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings.default')
 
 	from django.core.management import execute_from_command_line
-	print 'hello'
 	execute_from_command_line(sys.argv)
