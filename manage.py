14428112d36c5bcb21ceb01c4ed2da50ed34b655
Loknar <svefgud@gmail.com>
2015-04-04 00:39:58 +0000

    moved default settings

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 	if environment in ['development','testing','production']:
 		os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings.%s' % environment)
 	else:
-		os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings')
+		os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oneup.settings.default')
 
 	from django.core.management import execute_from_command_line
 	execute_from_command_line(sys.argv)
