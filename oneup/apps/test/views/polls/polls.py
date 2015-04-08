4165e87eed605eaabcbeedc991da8c0aafc8df4a
hannespetur <hannespetur@gmail.com>
2015-04-08 00:18:28 +0000

    further improved the polls

--- oneup/apps/test/views/polls/polls.py
+++ oneup/apps/test/views/polls/polls.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext, loader
 
 from ...models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
