94670ed515fbf1b8a153395c43734652aa0f8218
hannespetur <hannespetur@gmail.com>
2015-05-17 18:03:33 +0000

    part 4 of tutorial done

--- oneup/apps/tutorial/urls.py
+++ oneup/apps/tutorial/urls.py
@@ -4,11 +4,12 @@ from oneup.apps.tutorial.views import tutorial
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', tutorial.index, name='index'),
+    # url(r'^$', tutorial.index, name='index'),
+    url(r'^$', tutorial.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', tutorial.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', tutorial.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', tutorial.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', tutorial.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', tutorial.vote, name='vote'),
 ]
--- oneup/apps/tutorial/views/tutorial.py
+++ oneup/apps/tutorial/views/tutorial.py
@@ -1,24 +1,28 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from oneup.apps.test.models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'tutorial/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'tutorial/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'tutorial/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -36,4 +40,4 @@ def vote(request, question_id):
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
-    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    return HttpResponseRedirect(reverse('tutorial:results', args=(p.id,)))
