7d0c0601d1ead10bede47bc96cc6df8f297195cc
hannespetur <hannespetur@gmail.com>
2015-05-17 17:30:57 +0000

    moved some files around in tutorial app

--- oneup/apps/tutorial/urls.py
+++ oneup/apps/tutorial/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
-from oneup.apps.test.views import polls
+from oneup.apps.tutorial.views import tutorial
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', polls.index, name='index'),
+    url(r'^$', tutorial.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', polls.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', tutorial.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', polls.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', tutorial.results, name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', polls.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', tutorial.vote, name='vote'),
 ]
--- oneup/apps/tutorial/views/tutorial.py
+++ oneup/apps/tutorial/views/tutorial.py
@@ -8,17 +8,17 @@ from oneup.apps.test.models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'tutorial/index.html', context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'tutorial/detail.html', {'question': question})
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'tutorial/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -27,7 +27,7 @@ def vote(request, question_id):
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'tutorial/detail.html', {
             'question': p,
             'error_message': "You didn't select a choice.",
         })
