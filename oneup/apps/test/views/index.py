f0f755a1e3c8e17c014eae304d8218a75c11065a
Loknar <svefgud@gmail.com>
2015-05-17 15:26:29 +0000

    small refactor

--- oneup/apps/test/views/index.py
+++ oneup/apps/test/views/index.py
@@ -8,4 +8,4 @@ def page(request):
     team_name = "Team 1up"
     years_old = 15
     list_of_games = ["Counter Strike", "Dark Souls", "Dota 2", "Hearthstone", "Starcraft", "That mobile app game that ripped off Dota 2 heroes"]
-    return render(request, 'en/public/index.html', {"team": team_name, "years": years_old, "tab": "index", "games_array": list_of_games})
+    return render(request, 'en/public/index.html', {"team": team_name, "years": years_old, "index": "active", "games_array": list_of_games})
