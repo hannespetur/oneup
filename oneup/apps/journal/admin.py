5e26a3d3d397960e94000251474fee1cd7c735a2
Loknar <svefgud@gmail.com>
2015-05-25 15:20:33 +0000

    made changes to the journal model looks nice on admin page

--- oneup/apps/journal/admin.py
+++ oneup/apps/journal/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 
-from .models import Journal
+from .models import Journal, Category
 
 
 class JournalAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['title']}),
-        ('Date information', {'fields': ['posted_date'], 'classes': ['collapse']}),
+        ('Post title', {'fields': ['title']}),
+        ('Url string', {'fields': ['slug']}),
+        ('Date posted', {'fields': ['posted_date'], 'classes': ['collapse']}),
+        ('Body', {'fields': ['body']}),
+        ('Category', {'fields': ['category']}),
+        ('Visible', {'fields': ['visible']}),
     ]
     list_display = ('title', 'posted_date', 'was_posted_recently')
     list_filter = ['posted_date']
     search_fields = ['title']
 
 admin.site.register(Journal, JournalAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Category name', {'fields': ['name']}),
+        ('Url string', {'fields': ['slug']}),
+        ('Category description', {'fields': ['description']}),
+    ]
+    list_display = ('name', 'slug')
+    list_filter = ['slug']
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
--- oneup/apps/journal/models.py
+++ oneup/apps/journal/models.py
@@ -1,12 +1,21 @@
 import datetime
 
 from django.db import models
+from django.db.models import permalink
 from django.utils import timezone
 
 
 class Journal(models.Model):
-    title = models.CharField(max_length=200)
-    posted_date = models.DateTimeField('date posted')
+    title = models.CharField('Post title', max_length=200)
+    slug = models.SlugField('Url string', max_length=100, unique=True)
+    posted_date = models.DateTimeField('Date posted', default=datetime.datetime.now)
+    body = models.TextField('Post body', default='')
+    category = models.ForeignKey('journal.Category', blank=True, null=True)
+    visible = models.BooleanField(
+        'Visible',
+        default = False,
+        help_text = 'Check this to make the post visible on webpage'
+    )
 
     def was_posted_recently(self):
         now = timezone.now()
@@ -14,3 +23,26 @@ class Journal(models.Model):
 
     def __unicode__(self):
         return self.title
+
+    # see doc for get_absolute_url:
+    # https://docs.djangoproject.com/en/1.8/ref/models/instances/#get-absolute-url
+    @permalink
+    def get_absolute_url(self):
+        return '/journals/%s/%s' % (self.id, self.slug)
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField('Name', max_length=100, unique=True)
+    slug = models.SlugField('Slug', max_length=100, unique=True)
+    description = models.TextField('Description')
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+    # see doc for get_absolute_url:
+    # https://docs.djangoproject.com/en/1.8/ref/models/instances/#get-absolute-url
+    @permalink
+    def get_absolute_url(self):
+        return '/journals-by-category/%s' % self.slug
